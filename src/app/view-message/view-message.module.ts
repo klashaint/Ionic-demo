64dac455447bf0eacaff3335fbb4e9223eb406ff
devfemmy <odoyoakereleisrael@gmail.com>
2022-03-15 08:52:17 +0100

    feature: klasha integration

--- src/app/view-message/view-message.module.ts
+++ src/app/view-message/view-message.module.ts
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import { NgModule } from '@angular/core';
+import { AngularKlashaModule } from 'angular-klasha';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ViewMessagePage } from './view-message.page';
@@ -12,6 +14,7 @@ import { ViewMessagePageRoutingModule } from './view-message-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    AngularKlashaModule.forRoot('W2mbGtdx5vKCepFaUm2CqdzebaVW9z22shubB4xFbKTR3g4sL72+7qNQYHTUEfs0my1e/hAO1Nkdx9YbXTjUOg==','businessId',false),
     ViewMessagePageRoutingModule
   ],
   declarations: [ViewMessagePage]
--- src/app/view-message/view-message.page.ts
+++ src/app/view-message/view-message.page.ts
@@ -1,28 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DataService, Message } from '../services/data.service';
+import { KlashaOptions } from 'angular-klasha';
 
 @Component({
   selector: 'app-view-message',
   templateUrl: './view-message.page.html',
   styleUrls: ['./view-message.page.scss'],
 })
-export class ViewMessagePage implements OnInit {
-  public message: Message;
+export class ViewMessagePage  {
+  options: KlashaOptions = {
+    email: 'test@gmail.com',
+    phone_number: '+2348033582680',
+    merchantKey: 'W2mbGtdx5vKCepFaUm2CqdzebaVW9z22shubB4xFbKTR3g4sL72+7qNQYHTUEfs0my1e/hAO1Nkdx9YbXTjUOg==',
+    amount: 1000,
+    sourceCurrency: '',
+    destinationCurrency: '',
+    tx_ref: '' + Math.floor((Math.random() * 1000000000) + 1),
+    businessId: '1',
+    fullname: 'test name',
+    paymentDescription: '',
+    kit: {
+      currency: '',
+      phone_number: '2348033582680',
+      email: 'test@gmail.com',
+      fullname: 'test name',
+      tx_ref: '',
+      paymentType: '',
+    }
 
+  };
   constructor(
-    private data: DataService,
     private activatedRoute: ActivatedRoute
   ) { }
-
-  ngOnInit() {
-    const id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.message = this.data.getMessageById(parseInt(id, 10));
-  }
-
   getBackButtonText() {
     const win = window as any;
     const mode = win && win.Ionic && win.Ionic.mode;
     return mode === 'ios' ? 'Inbox' : '';
+  }
+  paymentInit() {
+    console.log('Payment initialized');
+  }
+  paymentCancel() {
+    console.log('Payment Cancelled');
+  }
+
+  paymentDone(info: any) {
+    console.log(info);
   }
 }
